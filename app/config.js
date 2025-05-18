d505c2771ac245a6aaa80bdd1836a309cc190055
Raul Insuasty <r.insuasty@uniandes.edu.co>
2025-05-18 12:39:48 -0500

    quitar nextjs

--- app/config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyAu-1qgHL7sSr3YErtFSWLTTFZLP1CiC0s",
-    authDomain: "qredi-auth.firebaseapp.com",
-    projectId: "qredi-auth",
-    storageBucket: "qredi-auth.firebasestorage.app",
-    messagingSenderId: "204030499494",
-    appId: "1:204030499494:web:6cb70167d26a1cd3e53267",
-    measurementId: "G-6QQ7V55LSV"
-};
